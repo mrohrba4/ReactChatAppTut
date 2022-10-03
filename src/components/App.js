bc1877926cbc2453d2324bb9e648c08a02909404
Michael Rohrbaugh <74413637+mrohrba4@users.noreply.github.com>
2022-10-03 14:28:22 -0400

    Add Login form functions

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Login from'./Login.js';
+
+function App() {
+  const[id, setId] = useState();
+
+  return (
+    <>
+      {id}
+      <Login onIdSubmit={setId} />
+    </> 
+  )
+}
+
+export default App;
