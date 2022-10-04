cb2b1e9b144f82b5bf1f00d9bea809c0d68a1fae
Michael Rohrbaugh <74413637+mrohrba4@users.noreply.github.com>
2022-10-04 15:23:47 -0400

    Add chat convo functionality.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useConversations } from '../contexts/ConversationsProvider'
+import OpenConversation from './OpenConversation'
 import Sidebar from './Sidebar'
 
 export default function Dashboard({ id }) {
-  return (
-    <div className="d-flex" style={{ height: '100vh' }}>
-        <Sidebar id={id} />
-    </div>
-  )
+    const { selectConversation } = useConversations()
+    return (
+        <div className="d-flex" style={{ height: '100vh' }}>
+            <Sidebar id={id} />
+            { selectConversation && <OpenConversation />}
+        </div>
+    )
 }
--- src/contexts/ConversationsProvider.js
+++ src/contexts/ConversationsProvider.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage'
 import { useContacts } from './ContactsProvider'
 
@@ -9,9 +9,10 @@ export function useConversations() {
   return useContext(ConversationsContext)
 }
 
-export default function ConversationsProvider({ children }) {
+export default function ConversationsProvider({ id, children }) {
   const [conversations, setConversations] = useLocalStorage('conversations', [])
   const { contacts } = useContacts()
+  const [selectConversationIndex, setSelectConversationIndex] = useState(0)
 
   function createConversation(recipients) {
     setConversations(prevConversations => {
@@ -19,7 +20,37 @@ export default function ConversationsProvider({ children }) {
     })
   }
 
-  const formattedConversations = conversations.map(conversation => {
+  function addMessageToConversation({ recipients, text, sender }) {
+    setConversations(prevConversations => {
+        let madeChange = false
+        const newMessage = { sender, text }
+        const newConversations = prevConversations.map(conversation => {
+            if (arrayEquality(conversation.recipients, recipients)) 
+            {
+                madeChange = true
+                return { 
+                    ...conversation, 
+                    messages: [ ...conversation.messages, newMessage]
+                }
+            }
+            return conversation
+        })
+
+        if (madeChange) {
+            return newConversations
+        } else {
+            return [
+                ...prevConversations, 
+                { recipients, messages: [newMessage] }]
+        }
+    })
+  }
+
+  function sendMessage(recipients, text) {
+    addMessageToConversation({ recipients, text, sender: id})
+  }
+
+  const formattedConversations = conversations.map((conversation, index) => {
     const recipients = conversation.recipients.map(recipient => {
         const contact = contacts.find(contact => {
             return contact.id === recipient
@@ -27,11 +58,15 @@ export default function ConversationsProvider({ children }) {
         const name = (contact && contact.name) || recipient
         return { id: recipient, name }
     })
-    return { ...conversation, recipients }
+    const selected = index === selectConversationIndex
+    return { ...conversation, recipients, selected }
   })
 
   const value = {
     conversations: formattedConversations,
+    selectConversation: formattedConversations[selectConversationIndex],
+    sendMessage,
+    selectConversationIndex: setSelectConversationIndex,
     createConversation
   }
 
@@ -40,4 +75,15 @@ export default function ConversationsProvider({ children }) {
       {children}
     </ConversationsContext.Provider>
   )
+}
+
+function arrayEquality(a, b) {
+    if (a.length !== b.length) return false
+
+    a.sort()
+    b.sort()
+
+    return a.every((element, index) => {
+        return element === b[index]
+    })
 }
