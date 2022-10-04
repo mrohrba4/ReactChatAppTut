b46ffb8b001775c515b42bad274e5962d75fa9c2
Michael Rohrbaugh <74413637+mrohrba4@users.noreply.github.com>
2022-10-04 11:37:36 -0400

    Add modals and button function.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
-import { Tab, Nav, Button } from 'react-bootstrap';
+import { Tab, Nav, Button, Modal } from 'react-bootstrap';
 import Conversations from './Conversations';
 import Contacts from './Contacts';
+import NewContactModal from './NewContactModal';
+import NewConversationModal from './NewConversationModal';
 
 const CONVERSATIONS_KEY = 'conversations';
 const CONTACTS_KEY = 'contacts';
 
 export default function Sidebar({ id }) {
     const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY);
+    const [modalOpen, setModalOpen] = useState(false);
     const conversationsOpen = activeKey === CONVERSATIONS_KEY; 
+
+    function closeModal() {
+        setModalOpen(false);
+    }
 
     return (
         <div style={{ width: '250px' }} className="d-flex flex-column">
@@ -35,10 +42,17 @@ export default function Sidebar({ id }) {
                 <div className='p-2 border-top border-end small'>
                     Your Id: <span className='text-muted'>{id}</span>
                 </div>
-                <Button>
+                <Button onClick={() => setModalOpen(true)} className="rounded-0">
                     New {conversationsOpen ? 'Conversation' : 'Contact'}
                 </Button>
             </Tab.Container>
+
+            <Modal show={modalOpen} onHide={closeModal}>
+                {conversationsOpen ? 
+                    <NewConversationModal closeModal={closeModal} /> :
+                    <NewContactModal closeModal={closeModal} />
+                }
+            </Modal>
         </div>
   )
 }
